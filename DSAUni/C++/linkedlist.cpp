4a19aef4e583e47760bad011a8388f136f0bb878
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-09-30 09:14:15 -0700

    c

--- DSAUni/C++/linkedlist.cpp
+++ DSAUni/C++/linkedlist.cpp
@@ -3,7 +3,41 @@
 using namespace std;
 
 class Linknode{
+public:
+    int data;
+    Linknode *next;
+    
+    Linknode(int val){
+        data = val;
+        next = nullptr;
+    }
 
+    void insert(int val){
+        Linknode *x = new Linknode(val);
+        x->next = this->next;
+        this->next = x;
+    }
+    
 
 
 };
+
+int main(){
+
+Linknode *x = new Linknode(5);
+x->insert(8);
+x->insert(22);
+x->insert(43);
+x->insert(99);
+x->insert(44);
+Linknode *y = x;
+while (y->next != nullptr)
+{
+    cout << y->data <<" ";
+    y = y->next; 
+}
+
+
+
+
+}
