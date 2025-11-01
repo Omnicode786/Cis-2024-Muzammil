28cf9a54baadba555b4f2ec17c290779ea2f8731
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-11-02 01:38:06 +0500

    c

--- DSAUni/C++/BinaryTrees/Tree.cpp
+++ DSAUni/C++/BinaryTrees/Tree.cpp
@@ -2,32 +2,27 @@
 
 using namespace std;
 
-class Tree{
-    public:
-        int data;
-        Tree* left;
-        Tree* right;
-    Tree (int value){
-// making the root of the tree
-data = value;
-right = NULL;
-left = NULL;
-
+class Tree
+{
+public:
+    int data;
+    Tree *left;
+    Tree *right;
+    Tree(int value)
+    {
+        // making the root of the tree
+        data = value;
+        right = NULL;
+        left = NULL;
     }
-
-        
-
-
-    
-
-
 };
 
- 
 int idx = -1;
-Tree* BuiltTree(vector<int>& sequence) {
+Tree *BuiltTree(vector<int> &sequence)
+{
     idx++;
-    if (sequence[idx] == -1) return NULL;
+    if (sequence[idx] == -1)
+        return NULL;
 
     Tree *root = new Tree(sequence[idx]);
     root->left = BuiltTree(sequence);
@@ -37,96 +32,156 @@ Tree* BuiltTree(vector<int>& sequence) {
 }
 int nodecount = 0;
 
-void TraverseTreePreOrder(Tree* tree){
-    if (tree == NULL) return ;
+void TraverseTreePreOrder(Tree *tree)
+{
+    if (tree == NULL)
+        return;
     cout << tree->data << " ";
     nodecount++;
     TraverseTreePreOrder(tree->left);
     TraverseTreePreOrder(tree->right);
-
 }
-void InOrder(Tree *tree){
-if (tree == NULL) return;
+void InOrder(Tree *tree)
+{
+    if (tree == NULL)
+        return;
 
-InOrder(tree->left);
-cout <<tree->data<<" ";
-InOrder(tree->right);
-
-}
-
-// post order 
-void PostOrder(Tree *tree){
-if (tree == NULL) return;
-
-PostOrder(tree->left);
-PostOrder(tree->right);
-cout <<tree->data<<" ";
-
+    InOrder(tree->left);
+    cout << tree->data << " ";
+    InOrder(tree->right);
 }
 
-// Level Order 
+// post order
+void PostOrder(Tree *tree)
+{
+    if (tree == NULL)
+        return;
 
-void LevelOrder(Tree *tree){
-queue <Tree*> q;
-q.push(tree);
-q.push(NULL);
-while (q.size() > 0){
-    Tree* curr = q.front();
-    q.pop();
+    PostOrder(tree->left);
+    PostOrder(tree->right);
+    cout << tree->data << " ";
+}
 
-    if (curr == NULL){
-        if (!q.empty()){
-            cout<<'\n';
-            q.push(NULL);
-            continue;
+// Level Order
+
+void LevelOrder(Tree *tree)
+{
+    queue<Tree *> q;
+    q.push(tree);
+    q.push(NULL);
+    while (q.size() > 0)
+    {
+        Tree *curr = q.front();
+        q.pop();
+
+        if (curr == NULL)
+        {
+            if (!q.empty())
+            {
+                cout << '\n';
+                q.push(NULL);
+                continue;
+            }
+            else
+            {
+                break;
+            }
         }
-        else{
-            break;
-        }
+
+        cout << curr->data << "  ";
+        if (curr->left != NULL)
+            q.push(curr->left);
+        if (curr->right != NULL)
+            q.push(curr->right);
     }
-
-    cout <<curr->data<<"  ";
-    if (curr->left != NULL) q.push(curr->left);
-    if (curr->right != NULL) q.push(curr->right);
-
-}
-    cout<<'\n';
-
-
+    cout << '\n';
 }
 
+int height(Tree *tree)
+{
+    if (tree == NULL)
+        return 0;
+    int left = height(tree->left);
+    int right = height(tree->right);
+
+    return max(right, left) + 1;
+}
+
+int count(Tree *tree)
+{
+    if (tree == NULL)
+    {
+        return 0;
+    }
+    int leftCount = count(tree->left);
+
+    int rightCount = count(tree->right);
+
+    return rightCount + leftCount + 1;
+}
 
 
+// sum of nodes
+// TC O(n)  
+int Sumation = 0;
+int Sum(Tree* tree){
+    if (tree == NULL) return 0;
+    Sumation = Sumation + tree->data;
+    Sum(tree->left);
+    Sum(tree->right);
 
-int main() {
+    return Sumation;
+
+}
+
+
+int main()
+{
     Tree *tree1 = new Tree(6);
-    cout << tree1->data<<' ';
-    // because the tree variable is essentially a pointer 
-    vector <int> seq = {5,3,2,-1,-1,4,-1,-1,10,22,1,3,-1,-1,4,-1,-1,9,-1,-1,33,2,-1,-1,-1};
+    cout << tree1->data << ' ';
+    // because the tree variable is essentially a pointer
+    vector<int> seq = {5, 3, 2, -1, -1, 4, -1, -1, 10, 22, 1, 3, -1, -1, 4, -1, -1, 9, -1, -1, 33, 2, -1, -1, -1};
     Tree *tree = BuiltTree(seq);
 
     // printing all left notes
-    Tree* a = tree;
+    Tree *a = tree;
 
-cout<<"PreOrder Traversal"<<"\n";
-     TraverseTreePreOrder(tree);
+    cout << "PreOrder Traversal" << "\n";
+    TraverseTreePreOrder(tree);
 
-
-
-     cout<<'\n'<<'\n';
-cout<<"InOrder Traversal"<<"\n";
+    cout << '\n'
+         << '\n';
+    cout << "InOrder Traversal" << "\n";
 
     InOrder(tree);
-     cout<<'\n'<<'\n';
-cout<<"PostOrder Traversal"<<"\n";
+    cout << '\n'
+         << '\n';
+    cout << "PostOrder Traversal" << "\n";
 
- PostOrder(tree);
-     cout<<'\n'<<'\n';
-    
-cout<<"Level ORder Traversal"<<"\n";
+    PostOrder(tree);
+    cout << '\n'
+         << '\n';
 
- LevelOrder(tree);
-     cout<<'\n'<<'\n';
+    cout << "Level ORder Traversal" << "\n";
 
-     cout<<nodecount;
-    }
+    LevelOrder(tree);
+    cout << '\n'
+         << '\n';
+
+    cout << nodecount;
+    cout << '\n'
+         << '\n';
+
+    cout << height(tree);
+
+    cout << '\n'
+         << '\n';
+
+    cout << count(tree);
+
+
+    cout << '\n';
+    cout <<Sum(tree);
+
+
+}
---
+++ DSAUni/C++/BinaryTrees/tempCodeRunnerFile.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Tree
+{
+public:
+    int data;
+    Tree *left;
+    Tree *right;
+    Tree(int value)
+    {
+        // making the root of the tree
+        data = value;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+int idx = -1;
+Tree *BuiltTree(vector<int> &sequence)
+{
+    idx++;
+    if (sequence[idx] == -1)
+        return NULL;
+
+    Tree *root = new Tree(sequence[idx]);
+    root->left = BuiltTree(sequence);
+    root->right = BuiltTree(sequence);
+
+    return root;
+}
+
+
+
+bool isSameTree(Tree* treeNo1, Tree* treeNo2){
+    if (treeNo1 == NULL || treeNo2 == NULL)
+{
+    return treeNo1== treeNo2;
+}
+
+bool isLeftSame = isSameTree(treeNo1->left,treeNo2->left);
+bool isRightSame = isSameTree(treeNo1->right, treeNo2->right);
+
+return isLeftSame && isRightSame && treeNo1->data == treeNo2->data;
+
+
+
+}
+
+int main(){
+
+    vector<int> treeSeq = {1,2,-1,-1,3,-1,-1};
+    Tree* p = BuiltTree(treeSeq);
+    Tree* q = BuiltTree(treeSeq);
+cout<<"HELLO"<<p->data<<'\n';
+cout<<"HELLO"<<q->data<<'\n';
+
+bool truthyFalse = isSameTree(p,q);
+cout<<truthyFalse;
+
+
+
+}
